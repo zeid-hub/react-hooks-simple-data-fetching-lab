ef9daf14f0245d21a4e27c3ffadb92038e38dca5
zeid <zeidhassan4@gmail.com>
2024-02-06 12:39:30 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+    const [img,setImg]=useState('')
+    const [isLoading,setIsLoading] = useState(false)
+    // let isLoading = false
+    useEffect(
+        ()=>
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res=>res.json())
+        .then(res=>{setImg(res.message)
+            setIsLoading(true)
+            console.log(img)})
+        
+        
+        
+    ,[])
+    if (!isLoading) return <p>is Loading ...</p>
+  return (
+        <img src={img} alt='A Random Dog'></img>
+  )
+}
+
+export default App
